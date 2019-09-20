f039c4ee086af528805cccaebffd956b2f99c4f2
karol <carolina.chamorro@securitasdirect.es>
2019-09-20 16:22:09 +0200

    Initial Commit

---
+++ acepta-un-reto/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  acepta-un-reto
+//
+//  Created by Carolina Chamorro on 20/09/2019.
+//  Copyright © 2019 Carolina Chamorro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
