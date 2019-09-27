b98d96e9d0340984c484e7904e4eb3474cea980d
karol <carolina.chamorro@securitasdirect.es>
2019-09-27 17:23:04 +0200

    feat: añadir iteracion para los casos de uso

--- acepta-un-reto/main.cpp
+++ acepta-un-reto/main.cpp
@@ -9,42 +9,39 @@
 #include <iostream>
 using namespace std;
 
-int getQuantity() {
+void getQuantity() {
     
-    unsigned int nCompartimentos, capacidadMayor, diferencia, i;
+    int nCompartimentos, capacidadMayor, diferencia;
     
-    cout << "Número de compartimentos, capacidad del más grande en litros y diferencia de litros de dos compartimentos adyacentes:";
-    
+    cout << "2";
     cin >> nCompartimentos >> capacidadMayor >> diferencia;
     
     while (nCompartimentos <= 0 || nCompartimentos > 10000) {
-        cout << "Tiene que ser > 0 y < que 10.000:";
+        cout << "ERROR 1";
         cin >> nCompartimentos;
     }
     
     while (capacidadMayor <= 0 || capacidadMayor > 10000) {
-        cout << "Tiene que ser > 0 y < que 10.000:";
+        cout << "ERROR 2";
         cin >> capacidadMayor;
     }
     
     while (diferencia <= 0 || diferencia > 10000) {
-        cout << "Tiene que ser > 0 y < que 10.000:";
+        cout << "ERROR 3";
         cin >> diferencia;
     }
     
-    // Total de litros
     int total = capacidadMayor;
     int aux = 0;
     ++aux+= capacidadMayor;
     
-    for (i = 0; i < nCompartimentos - 1; i++) {
+    for (int i = 0; i < nCompartimentos - 1; i++) {
         int add = aux - diferencia;
         total += add - 1;
         aux = add;
-        
     }
     
-    return total;
+    cout << total;
     
 }
 
@@ -52,16 +49,15 @@ int getQuantity() {
 
 int main() {
     
-    unsigned int numCasos, i;
+    unsigned int numCasos;
     
-    cout << "Número de casos: ";
+    cout << "1: ";
     cin >> numCasos;
+    cout << numCasos;
     
-    if ( numCasos > 0) {
-        for (i = 0; i < numCasos; ++i) {
-            int num = 0;
-            num = getQuantity();
-            cout << num;
+    if (numCasos > 0) {
+        for (int i = 0; i < numCasos; ++i) {
+            getQuantity();
         }
     }
     
